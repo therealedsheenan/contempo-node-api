a1950d6ecbea1552d646d68258f356a88bbfa6b9
Sheenan Tenepre <sheenantenepre@gmail.com>
2017-03-31 15:04:56 +0900

    partial dev environment

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
 // main file
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const cors = require('cors')
+
+//routes
+const index= require('./src/routes/index')
+
+// initialization
+const app = express()
+const dbUrl = `mongodb://${process.env.USERNAME}:${process.env.PASSWORD}@127.0.0.1`;
+
+// DB Setup
+mongoose.connect(dbUrl)
+
+// App Setup
+app.use(morgan('combined'))
+app.use(cors())
+app.use(bodyParser.json({ type: '*/*' }))
+
+// sample route
+index(app)
+
+module.exports = app
---
+++ src/routes/index.js
@@ -0,0 +1,8 @@
+
+const index = function (app) {
+  app.get('/api/', function (req, res) {
+    res.send({ message: 'Hello from index!' })
+  })
+}
+
+module.exports = index
